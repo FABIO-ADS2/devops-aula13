14a1aeccf2da8e8a68e6ff802d234c1783bbb443
FABIO-ADS2 <30833025+FABIO-ADS2@users.noreply.github.com>
2017-11-08 21:16:03 -0200

    Update models.py

--- app/models.py
+++ app/models.py
@@ -19,4 +19,9 @@ class Candidato(models.Model):
     cpf = models.CharField(max_length=20)
     endereco = models.CharField(max_length=200)
     telefone = models.CharField(max_length=9)
-    
+
+class Local_Prova(models.Model):
+    nome_escola = models.CharField(max_length=200)
+    endereco = models.CharField(max_length=200)
+    cidade = models.CharField(max_length=200)
+    estado = models.CharField(max_length=200)
